11feb961fde21a6b1e89fbfa2271cd4b8591673b
Alexey Deryugin <alederu7891@gmail.com>
2018-09-17 04:41:07 +0300

    Задание 4 легкое и нормальное

--- lesson04/home_work/hw04_easy.py
+++ lesson04/home_work/hw04_easy.py
@@ -6,9 +6,26 @@
 # квадратами элементов исходного списка
 # [1, 2, 4, 0] --> [1, 4, 16, 0]
 
+original_list = [1, 2, 4, 0]
+new_list = list(map(lambda x:x**2, original_list[:]))
+
+print('list_1:', original_list)
+print('list_2:', new_list)
+
+# с генератором
+newest_list = [el**2 for el in original_list[:]]
+
+print('list_1:', original_list)
+print('list_2:', newest_list)
+
 # Задание-2:
 # Даны два списка фруктов.
 # Получить список фруктов, присутствующих в обоих исходных списках.
+
+fruit_list_1 = ['Апельсин', 'Мандарин', 'Яблоко', 'Киви', 'Лимон']
+fruit_list_2 = ['Манго', 'Грейпфрут', 'Апельсин', 'Банан', 'Лимон', 'Киви']
+result_list = [i for i in fruit_list_1 if i in fruit_list_2]
+print(result_list)
 
 # Задание-3:
 # Дан список, заполненный произвольными числами.
@@ -16,3 +33,10 @@
 # + Элемент кратен 3
 # + Элемент положительный
 # + Элемент не кратен 4
+
+from random import randint
+
+int_list = [randint(-100, 100) for j in range(0, 10)]
+conditioned_list = [i for i in int_list if i >= 0 and i%4 and not i%3]
+print(int_list)
+print(conditioned_list)
--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -20,6 +20,28 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
+# c re
+import re
+
+lc = re.compile('[a-z]+')
+lowercase = lc.findall(line)
+print(lowercase)
+
+
+# без re
+
+list_1 = list(line[:])
+list_2 = []
+for i in list_1:
+    if i.isupper():
+        i = "_"
+    list_2.append(i) # вместо прописных нижнее подчеркивание, буквы отдельно
+
+list_4 = ''.join(list_2).split('_') # по нижнему подчеркиванию разбито на последовательность текстов
+
+line_low_1 = [i for i in list_4 if i != ''] # убраны пустые значения в листе
+print(line_low_1)
+
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
@@ -45,9 +67,32 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+line_2_cond = re.findall(r'[a-z]{2}([A-Z]+)[A-Z]{2}', line_2)
+print(line_2_cond)
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+import random
+from itertools import groupby
+
+a = (random.randint(0, 9) for _ in range(2500))
+a = ''.join(list(map(lambda x: str(x), a)))
+# print(a)
+# Запись в файл
+path = 'script' + '.txt'
+with open(path, 'w', encoding='UTF-8') as file:
+       file.write(str(a))
+
+# Чтение файла
+with open(path, 'r', encoding='UTF-8') as file:
+       string_for_file = list(file.read())
+print(string_for_file)
+
+# Поиск самой длинной последовательности одинаковых цифр
+b = max((list(g) for k, g in groupby(a)), key=len)
+print(b)
